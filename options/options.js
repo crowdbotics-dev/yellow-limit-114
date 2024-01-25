a19a35af7cac24892fe9a07b8705439dd84caaaa
Crowdbotics <engineering@crowdbotics.com>
2024-01-25 06:03:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-limit-114",
+  url: "https://yellow-limit-114.botics.co",
+  api: "https://yellow-limit-114.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
