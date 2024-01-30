23966c9dd367d8de341a98c1e8e2c7c250b7e4e9
ravinder kumar <ravinder.y+1@crowdbotics.com>
2024-01-30 16:54:33 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "yellow-limit-114",
   url: "https://yellow-limit-114.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
