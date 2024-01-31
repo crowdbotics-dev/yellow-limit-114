71806bc5701a2ec7a6bf3db69cad82413db110b1
ravinder kumar <ravinder.y+1@crowdbotics.com>
2024-01-31 15:35:44 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,51 +1,29 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Image, TextInput, Button } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
-const LoginScreen = () => {
+const DotScreen = () => {
   return <SafeAreaView style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={{
-        uri: 'https://tinyurl.com/42evm3m3'
-      }} />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Login" color="#841584" />
-      </View>
+      <View style={styles.dot} />
+      <View style={styles.dot} />
+      <View style={styles.dot} />
+      <View style={styles.dot} />
+      <View style={styles.dot} />
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  logoContainer: {
-    marginBottom: 50
-  },
-  logo: {
-    width: 150,
-    height: 150
-  },
-  inputContainer: {
-    width: '80%',
-    marginBottom: 20
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingLeft: 10,
-    borderRadius: 5
-  },
-  buttonContainer: {
-    width: '80%'
+  dot: {
+    width: 10,
+    height: 10,
+    backgroundColor: "white",
+    borderRadius: 5,
+    margin: 10
   }
 });
-export default LoginScreen;
+export default DotScreen;
