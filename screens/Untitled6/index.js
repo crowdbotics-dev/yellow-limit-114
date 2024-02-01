9cb975128556fef9fa4f0136b101055c78b020e6
ravinder kumar <ravinder.y+1@crowdbotics.com>
2024-02-01 15:42:41 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -1,3 +1,6 @@
+import { FlatList } from "react-native";
+import { View } from "react-native";
+import { CheckBox } from "react-native-elements";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +11,35 @@ const Untitled6 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><CheckBox style={styles.YrFINkQl} title="Checkbox Title"></CheckBox><FlatList style={styles.MstpXUEh} renderItem={({
+        item
+      }) => <View style={styles.lQdRDPVL}></View>} ItemSeparatorComponent={() => <View style={styles.FljafvmV} />} data={[1, 2, 3]} keyExtractor={(item, index) => index}></FlatList></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  YrFINkQl: {
+    width: 183,
+    height: 69
+  },
+  MstpXUEh: {
+    position: "absolute",
+    width: 154,
+    height: 198,
+    left: 28,
+    top: 99
+  },
+  lQdRDPVL: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#FFFFFF"
+  },
+  FljafvmV: {
+    backgroundColor: "#000000",
+    height: 1
   }
 });
 export default Untitled6;
